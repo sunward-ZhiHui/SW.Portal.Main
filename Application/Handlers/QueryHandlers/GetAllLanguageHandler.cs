7d97cf2d6ab7577bd050780d2d8722aaec7f58b6
pakaruppusamy <pakaruppusamy@gmail.com>
2023-05-02 14:32:16 +0530

    Commit Changes

---
+++ Application/Handlers/QueryHandlers/GetAllLanguageHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Queries;
+using Core.Entities;
+using Core.Repositories.Query;
+using Core.Repositories.Query.Base;
+using MediatR;
+using System;
+using System.Linq;
+
+namespace CMS.Application.Handlers.QueryHandlers
+{
+    public class GetAllLanguageHandler : IRequestHandler<GetAllLanguageQuery, List<LanguageMaster>>
+    {
+        private readonly IQueryRepository<LanguageMaster> _queryRepository;
+        public GetAllLanguageHandler(IQueryRepository<LanguageMaster> queryRepository)
+        {
+            _queryRepository= queryRepository;
+        }
+        public async Task<List<LanguageMaster>> Handle(GetAllLanguageQuery request, CancellationToken cancellationToken)
+        {
+            return (List<LanguageMaster>)await _queryRepository.GetListAsync();
+        }
+    }
+}

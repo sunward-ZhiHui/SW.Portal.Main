65a8c7eccfa14b8fb673025199177b421ba29e82
Vijay <Vijay@LAPTOP-HDA4JTJH>
2023-06-05 10:32:51 +0530

    Date format

---
+++ Application/Common/DateFormatHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common
+{
+    public static class DateFormatHelper
+    {
+        public static string FormatDateTime(DateTime dateTime)
+        {
+            // Define your desired date format here
+            return dateTime.ToString("dd-MMM-yyyy HH:MM:ss");
+        }
+    }
+}

d4c8c23ccc28db2620b4c867a6fa4fb81343cf88
pakaruppusamy <pakaruppusamy@gmail.com>
2023-07-10 13:49:20 +0530

    Commit NavItems Repository

---
+++ Application/Queries/GetAllNavItemsQuery.cs
@@ -0,0 +1,27 @@
+﻿using Application.Queries.Base;
+using Core.Entities;
+using Core.Entities.Views;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries
+{
+    public class GetAllNavItemsQuery : PagedRequest, IRequest<List<Navitems>>
+    {
+        public string SearchString { get; set; }
+    }
+
+    public class GetAllNavItemsItemSerialNoQuery : PagedRequest, IRequest<Navitems>
+    {
+        public string SearchString { get; set; }
+        public string ItemSerialNo { get; set; }
+        public GetAllNavItemsItemSerialNoQuery(string ItemSerialNo)
+        {
+            this.ItemSerialNo = ItemSerialNo;
+        }
+    }
+}
---
+++ Infrastructure/Repository/Query/NavItemsQueryRepository.cs
@@ -0,0 +1,57 @@
+﻿using Core.Repositories.Query;
+using Infrastructure.Repository.Query.Base;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.EntityModel;
+using Core.Entities.Views;
+using Core.Entities;
+
+namespace Infrastructure.Repository.Query
+{
+    public class NavItemsQueryRepository : QueryRepository<Navitems>, INavItemsQueryRepository
+    {
+        public NavItemsQueryRepository(IConfiguration configuration)
+            : base(configuration)
+        {
+
+        }
+        public async Task<IReadOnlyList<Navitems>> GetAllAsync()
+        {
+            try
+            {
+                var query = "select  * from Navitems";
+
+                using (var connection = CreateConnection())
+                {
+                    return (await connection.QueryAsync<Navitems>(query)).ToList();
+                }
+            }
+            catch (Exception exp)
+            {
+                throw new Exception(exp.Message, exp);
+            }
+        }
+        public async Task<Navitems> GetByItemSerialNoAsync(string ItemSerialNo)
+        {
+            try
+            {
+                var query = "SELECT * FROM Navitems WHERE ItemSerialNo!=null and ItemSerialNo =" + ItemSerialNo;
+                using (var connection = CreateConnection())
+                {
+                    return (await connection.QueryFirstOrDefaultAsync<Navitems>(query));
+                }
+            }
+            catch (Exception exp)
+            {
+                throw new Exception(exp.Message, exp);
+            }
+        }
+
+    }
+}

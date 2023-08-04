579fbeae5407983d59a668993a0ce0fa90b51cb6
sivaranjani <sivaranjaniditto@gmail.com>
2023-08-04 16:37:35 +0530

    commit

---
+++ Core/Repositories/Query/IEmailActivityCatgorysQueryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Core.Entities.Views;
+using Core.Repositories.Query.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories.Query
+{
+    public interface IEmailActivityCatgorysQueryRepository : IQueryRepository<EmailActivityCatgorys>
+    {
+        Task<IReadOnlyList<EmailActivityCatgorys>> GetAllAsync();
+        Task<long> Insert(EmailActivityCatgorys emailActivityCatgorys);
+    }
+}

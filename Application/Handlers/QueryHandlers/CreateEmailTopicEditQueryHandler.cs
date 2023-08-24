82bb04c771593333418a049e135e3acb23464513
Madhu <madhuditto@gmail.com>
2023-08-23 17:41:52 -0700

    NewCreateEmail Changes

---
+++ Application/Handlers/QueryHandlers/CreateEmailTopicEditQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Queries;
+using Core.Entities;
+using Core.Repositories.Query.Base;
+using Core.Repositories.Query;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.QueryHandlers
+{
+    public class CreateEmailTopicEditQueryHandler : IRequestHandler<CreateEmailTopicsEditQuery, long>
+    {
+        private readonly ICreateEmailTopicEditQueryRepository _topicTodoListQueryRepository;
+        public CreateEmailTopicEditQueryHandler(ICreateEmailTopicEditQueryRepository topicTodoListQueryRepository)
+        {
+            _topicTodoListQueryRepository = topicTodoListQueryRepository;
+        }
+
+        public async Task<long> Handle(CreateEmailTopicsEditQuery request, CancellationToken cancellationToken)
+        {
+            var req = await _topicTodoListQueryRepository.Update(request);
+            return req;
+        }
+    }
+    public class CreateEmailTopicUploadQueryHandler : IRequestHandler<CreateEmailUploadQuery, List<Documents>>
+    {
+        private readonly ICreateEmailTopicEditQueryRepository _topicTodoListQueryRepository;
+        public CreateEmailTopicUploadQueryHandler(ICreateEmailTopicEditQueryRepository topicTodoListQueryRepository)
+        {
+            _topicTodoListQueryRepository = topicTodoListQueryRepository;
+        }
+
+        public async Task<List<Documents>> Handle(CreateEmailUploadQuery request, CancellationToken cancellationToken)
+        {
+            return (List<Documents>) await _topicTodoListQueryRepository.GetAllAsync(request.sessionId);
+           
+        }
+    }
+}
---
+++ Application/Queries/CreateEmailTopicsEditQuery.cs
@@ -0,0 +1,24 @@
+﻿using Application.Queries.Base;
+using Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries
+{
+    public class CreateEmailTopicsEditQuery : EmailTopics, IRequest<long>
+    {
+    }
+    public class CreateEmailUploadQuery : PagedRequest, IRequest<List<Documents>>
+    {
+        public Guid? sessionId { get; set; }
+        public CreateEmailUploadQuery(Guid? id)
+        {
+            this.sessionId = id;
+        }
+    }
+
+}

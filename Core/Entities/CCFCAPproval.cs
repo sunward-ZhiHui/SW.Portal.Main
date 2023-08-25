7b9b4cd1daf6175744ae40188a0a865bf1f626f9
siva ranjani <sivaranjaniditto@gmail.com>
2023-08-25 16:30:51 +0530

    commit

---
+++ Core/Entities/CCFCAPproval.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class CCFCAPproval:BaseEntity
+    {
+        [Key]
+        public long CCFCAPprovalID { get; set; }
+        public bool? IsApproved { get; set; }
+        public bool? IsNotApproved { get; set; }
+
+        public string? Comments { get; set; }
+        public long? VerifiedBy { get; set; }
+        public DateTime? ApprovalDate { get; set; }
+    }
+}

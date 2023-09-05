d69b23a4a98f41c4f6b4419b40466d322c7654ad
Vijay <Vijay@LAPTOP-HDA4JTJH>
2023-09-05 12:58:16 +0530

    login api controller imp

---
+++ SW.Portal.Solutions/Controllers/ApplicationUserController.cs
@@ -0,0 +1,35 @@
+﻿using Core.Repositories.Query;
+using Infrastructure.Repository.Query;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SW.Portal.Solutions.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : Controller
+    {
+        private readonly IApplicationUserQueryRepository _applicationUserQueryRepository;
+
+        public ApplicationUserController(IApplicationUserQueryRepository applicationUserQueryRepository)
+        {
+            _applicationUserQueryRepository = applicationUserQueryRepository;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(string loginId, string password)
+        {
+            var response = await _applicationUserQueryRepository.LoginAuth(loginId, password);
+            if (response != null)
+                return Ok(response);
+            else
+                return NotFound("Invalid User");
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Test";
+        }
+    }
+}

74c5cdc23786193fb771ce6fb68b3f4de05f2e99
Vijay <Vijay@LAPTOP-HDA4JTJH>
2023-09-22 11:13:09 +0530

    email changes

---
+++ AC.SD.Core/wwwroot/js/firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+﻿// firebase-messaging-sw.js
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js");
+
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onMessage((payload) => {
+    console.log("Message received:", payload);
+    const { title, body } = payload.notification;
+    self.registration.showNotification(title, {
+        body,
+    });
+});

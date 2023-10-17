7c60ab19cc6f118286182d29cb70e91ab23ce6ef
Vijay <Vijay@LAPTOP-HDA4JTJH>
2023-10-17 12:55:06 +0530

    tage delete option imp

--- Core/Entities/EmailActivityCatgorys.cs
+++ Core/Entities/EmailActivityCatgorys.cs
@@ -27,6 +27,7 @@ namespace Core.Entities
         public DateTime? ModifiedDate { get; set; }
         public Guid? SessionId { get; set; }
         public int? StatusCodeID { get; set; }
+        public bool? IsTagDelete { get; set; }
         public long? AddedByUserID { get; set; }
         [NotMapped]
         public string ModifiedBy { get; set; }

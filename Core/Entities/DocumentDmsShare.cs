56680b7ad76ba1fcd61cf94c6f3e267ec8373546
pakaruppusamy@gmail.com <pakaruppusamy@gmail.com>
2023-10-19 16:49:28 +0530

    Commit Changes

--- Core/Entities/DocumentDmsShare.cs
+++ Core/Entities/DocumentDmsShare.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities.Base;
+using Core.Entities.CustomValidations;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -16,8 +17,10 @@ namespace Core.Entities
         public long? DocumentId { get; set; }
         public Guid? DocSessionId { get; set; }
         public bool IsExpiry { get; set; } = false;
+        [FileProfileTypeCustomValidation]
         public DateTime? ExpiryDate { get; set; }
         public bool IsDeleted { get; set; } = false;
+        public string? Description { get; set; }
         [NotMapped]
         public string? FileName { get; set; }
 

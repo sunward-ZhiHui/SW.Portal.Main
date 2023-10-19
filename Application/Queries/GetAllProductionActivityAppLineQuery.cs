c6b7dbf388a210c3fcb8b5f70c85eec9346e5205
siva ranjani <sivaranjaniditto@gmail.com>
2023-10-19 17:06:17 +0530

    commit production activity

---
+++ Application/Queries/GetAllProductionActivityAppLineQuery.cs
@@ -0,0 +1,18 @@
+﻿
+using Application.Queries.Base;
+using Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries
+{
+   
+     public class GetAllProductionActivityAppLineQuery : PagedRequest, IRequest<List<ProductionActivityAppLine>>
+    {
+       
+    }
+}
---
+++ Infrastructure/Repository/Query/ProductionActivityAppLineQueryRepository.cs
@@ -0,0 +1,42 @@
+﻿using Core.Entities;
+using Core.Repositories.Query;
+using Dapper;
+using Infrastructure.Repository.Query.Base;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Query
+{
+    public class ProductionActivityAppLineQueryRepository : QueryRepository<ProductionActivityAppLine>, IProductionActivityQueryRepository
+    {
+        public ProductionActivityAppLineQueryRepository(IConfiguration configuration)
+            : base(configuration)
+        {
+
+        }
+
+        public async Task<IReadOnlyList<ProductionActivityAppLine>> GetAllAsync()
+        {
+            try
+            {
+             
+                var query = "select  AMC.Value as Process,AMD.Value as Result,AMC1.Value as Category,AMC2.Value AS Action from ProductionActivityAppLine as PAAL inner Join ApplicationMasterChild as AMC ON AMC.ApplicationMasterChildID = PAAL.ManufacturingProcessChildID \r\ninner Join ApplicationMasterChild as AMC1 ON AMC1.ApplicationMasterChildID = PAAL.ProdActivityCategoryChildID inner Join ApplicationMasterChild as AMC2 ON AMC2.ApplicationMasterChildID = PAAL.ProdActivityActionChildD inner join ApplicationMasterDetail as AMD ON AMD.ApplicationMasterDetailID = PAAL.ProdActivityResultID";
+
+                using (var connection = CreateConnection())
+                {
+                    return (await connection.QueryAsync<ProductionActivityAppLine>(query)).ToList();
+                }
+            }
+            catch (Exception exp)
+            {
+                throw new Exception(exp.Message, exp);
+            }
+        }
+
+    
+    }
+}

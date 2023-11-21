e8efdb8b006e68c90a272842703bbf9c11db2a3d
pakaruppusamy@gmail.com <pakaruppusamy@gmail.com>
2023-11-21 09:22:10 +0530

    Commit Employee Chnages

--- Infrastructure/Repository/Query/EmployeeQueryRepository.cs
+++ Infrastructure/Repository/Query/EmployeeQueryRepository.cs
@@ -24,7 +24,16 @@ namespace Infrastructure.Repository.Query
         }
         private string QueryString()
         {
-            var query = "SELECT concat(emp.FirstName,',',emp.LastName) as Name, emp.EmployeeID, emp.UserID, emp.SageID,emp.PlantID, emp.LevelID, emp.LanguageID, emp.CityID, emp.RegionID, emp.ReportID,\r\nemp.FirstName, emp.NickName, emp.LastName, emp.Gender, emp.JobTitle, emp.Email, u.LoginID, u.LoginPassword,u.UserCode,\r\nemp.TypeOfEmployeement, emp.Signature, emp.ImageUrl, emp.DateOfEmployeement,emp.LastWorkingDate, emp.Extension, emp.SpeedDial, emp.SkypeAddress, emp.Mobile,\r\nemp.IsActive, emp.SectionID,  emp.DivisionID,emp.DesignationId, emp.DepartmentId, emp.SubSectionId, emp.SubSectionTid, u.StatusCodeID,\r\n emp.AddedByUserId, emp.ModifiedByUserId,emp.AddedDate, emp.ModifiedDate, emp.AcceptanceStatus, emp.ExpectedJoiningDate, emp.AcceptanceStatusDate, emp.HeadCount,\r\na.UserName as AddedByUser,\r\nmo.UserName as ModifiedByUser,\r\nag.Value as Status,\r\nu.SessionID,\r\nu.InvalidAttempts,\r\nu.Locked\r\nFROM Employee emp\r\nLEFT JOIN ApplicationUser u ON u.userId = emp.userId\r\nLEFT JOIN ApplicationUser a ON a.UserId = emp.AddedByUserID\r\nLEFT JOIN ApplicationUser mo ON mo.UserId = emp.ModifiedByUserID\r\nLEFT JOIN ApplicationMasterDetail ag ON ag.ApplicationMasterDetailID = emp.AcceptanceStatus\r\nWHERE ag.Value!='Resign' or ag.Value is null";
+            var query = @"SELECT concat(emp.FirstName,',',emp.LastName) as Name, emp.EmployeeID, emp.UserID, emp.SageID,emp.PlantID, emp.LevelID, emp.LanguageID, emp.CityID, emp.RegionID, emp.ReportID, emp.FirstName, emp.NickName, emp.LastName, emp.Gender, emp.JobTitle, emp.Email, u.LoginID, u.LoginPassword,u.UserCode,emp.TypeOfEmployeement, emp.Signature, emp.ImageUrl, emp.DateOfEmployeement,emp.LastWorkingDate, emp.Extension, emp.SpeedDial, emp.SkypeAddress, emp.Mobile,emp.IsActive, emp.SectionID,  emp.DivisionID,emp.DesignationId, emp.DepartmentId, emp.SubSectionId, emp.SubSectionTid, u.StatusCodeID,  emp.AddedByUserId, emp.ModifiedByUserId,emp.AddedDate, emp.ModifiedDate, emp.AcceptanceStatus, emp.ExpectedJoiningDate, emp.AcceptanceStatusDate, emp.HeadCount, a.UserName as AddedByUser, mo.UserName as ModifiedByUser, ag.Value as Status, u.SessionID, u.InvalidAttempts, 
+                u.Locked,d.Name as DesignationName,p.PlantCode as CompanyName
+                FROM Employee emp 
+                LEFT JOIN ApplicationUser u ON u.userId = emp.userId
+                LEFT JOIN ApplicationUser a ON a.UserId = emp.AddedByUserID 
+                LEFT JOIN ApplicationUser mo ON mo.UserId = emp.ModifiedByUserID 
+                LEFT JOIN ApplicationMasterDetail ag ON ag.ApplicationMasterDetailID = emp.AcceptanceStatus 
+                LEFT JOIN plant p ON p.plantId = emp.plantId 
+                LEFT JOIN Designation d ON d.DesignationID = emp.DesignationID
+                WHERE ag.Value!='Resign' or ag.Value is null";
             return query;
         }
         public async Task<IReadOnlyList<ViewEmployee>> GetAllByStatusAsync()
@@ -45,30 +54,13 @@ namespace Infrastructure.Repository.Query
         }
         public async Task<IReadOnlyList<ViewEmployee>> GetAllAsync()
         {
-            List<ViewEmployee> ViewEmployees = new List<ViewEmployee>();
             try
             {
                 var query = QueryString();
 
                 using (var connection = CreateConnection())
                 {
-                    var result = (await connection.QueryAsync<ViewEmployee>(query)).ToList();
-                    if (result == null || result.Count == 0)
-                    {
-
-                    }
-                    else
-                    {
-                        result.ForEach(s =>
-                        {
-                            var empCount = ViewEmployees.Where(w => w.EmployeeID == s.EmployeeID).Count();
-                            if (empCount == 0)
-                            {
-                                ViewEmployees.Add(s);
-                            }
-                        });
-                    }
-                    return ViewEmployees;
+                    return (await connection.QueryAsync<ViewEmployee>(query)).ToList();
                 }
             }
             catch (Exception exp)
@@ -78,30 +70,13 @@ namespace Infrastructure.Repository.Query
         }
         public async Task<IReadOnlyList<ViewEmployee>> GetAllUserAsync()
         {
-            List<ViewEmployee> ViewEmployees = new List<ViewEmployee>();
             try
             {
                 var query = QueryString();
 
                 using (var connection = CreateConnection())
                 {
-                    var result = (await connection.QueryAsync<ViewEmployee>(query)).ToList();
-                    if (result == null || result.Count == 0)
-                    {
-
-                    }
-                    else
-                    {
-                        result.ForEach(s =>
-                        {
-                            var empCount = ViewEmployees.Where(w => w.EmployeeID == s.EmployeeID).Count();
-                            if (empCount == 0)
-                            {
-                                ViewEmployees.Add(s);
-                            }
-                        });
-                    }
-                    return ViewEmployees;
+                    return (await connection.QueryAsync<ViewEmployee>(query)).ToList();
                 }
             }
             catch (Exception exp)
@@ -115,28 +90,29 @@ namespace Infrastructure.Repository.Query
             try
             {
                 var query = "select  * from view_GetEmployee";
-
+                var result = new List<ViewEmployee>();
                 using (var connection = CreateConnection())
                 {
-                    var result = (await connection.QueryAsync<ViewEmployee>(query)).ToList();
-                    if (result == null || result.Count == 0)
-                    {
-
-                    }
-                    else
-                    {
-                        result.ForEach(s =>
-                        {
-                            var empCount = ViewEmployees.Where(w => w.EmployeeID == s.EmployeeID).Count();
-                            if (empCount == 0)
-                            {
-                                s.Locked = s.Locked == true ? true : false;
-                                ViewEmployees.Add(s);
-                            }
-                        });
-                    }
-                    return ViewEmployees;
+                    result = (await connection.QueryAsync<ViewEmployee>(query)).ToList();
                 }
+                if (result == null || result.Count == 0)
+                {
+
+                }
+                else
+                {
+                    result.ForEach(s =>
+                    {
+                        var empCount = ViewEmployees.Where(w => w.EmployeeID == s.EmployeeID).Count();
+                        if (empCount == 0)
+                        {
+                            s.Locked = s.Locked == true ? true : false;
+                            ViewEmployees.Add(s);
+                        }
+                    });
+                }
+                return ViewEmployees;
+
             }
             catch (Exception exp)
             {

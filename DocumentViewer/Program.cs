33443b27133643eb6956a62cb20fc1bfed5f817b
pakaruppusamy@gmail.com <pakaruppusamy@gmail.com>
2024-01-10 17:53:05 +0530

    commit Changes

--- DocumentViewer/Program.cs
+++ DocumentViewer/Program.cs
@@ -38,6 +38,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.Use((context, next) => { context.Request.EnableBuffering(); return next(); });
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();

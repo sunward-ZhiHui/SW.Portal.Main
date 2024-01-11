99c86080efa6dbd55caec4828c4b55c8dbd66062
sivaranjani <sivaranjaniditto@gmail.com>
2024-01-11 10:48:12 +0530

    Commit Service Change

--- SW.Portal.Solutions/Models/LoginModel.cs
+++ SW.Portal.Solutions/Models/LoginModel.cs
@@ -5,4 +5,12 @@
         public string? loginId { get; set; }
         public string? password { get; set; }
     }
+
+    public class DeviceTkenModel
+    {
+        public string? LoginId { get; set; }
+        public string? DeviceType { get; set; }
+        public string? TokenID { get; set; }
+    }
+
 }

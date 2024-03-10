bfb9c2a89e20364a1c7531c52a4ec89ccbe93ce2
Vijay <Vijay@LAPTOP-HDA4JTJH>
2024-03-10 12:10:18 +0530

    bi dashboard

--- SW.Portal.Solutions/Controllers/DefaultDashboardController.cs
+++ SW.Portal.Solutions/Controllers/DefaultDashboardController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SW.Portal.Solutions.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class DefaultDashboardController : DashboardController
     {
         public DefaultDashboardController(DashboardConfigurator configurator, IDataProtectionProvider? dataProtectionProvider = null)

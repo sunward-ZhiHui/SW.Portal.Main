ef1b79c5cccb125116c27644ef96f4f1fa3b0e79
sivaranjani <sivaranjaniditto@gmail.com>
2024-04-02 17:11:20 +0530

    commit Changes For API

---
+++ SW.Portal.Solutions/Models/ProductionActivityAppModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SW.Portal.Solutions.Models
+{
+    public class ProductionActivityAppModel
+    {
+        [Key]
+        public long ProductionActivityAppID { get; set; }
+
+        public long? CompanyID { get; set; }
+
+        public string? ProdOrderNo { get; set; }
+        public string? Comment { get; set; }
+        public string? TopicID { get; set; }
+        public long? LocationID { get; set; }
+        [NotMapped]
+        public long? ICTMasterID { get; set; }
+        [NotMapped]
+        public string? DeropdownName { get; set; }
+    }
+}

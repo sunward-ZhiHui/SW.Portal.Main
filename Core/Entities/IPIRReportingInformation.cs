a4e7a878723fa4b8ab923aeb4ec3d87457362989
sivaranjani <sivaranjaniditto@gmail.com>
2024-08-06 15:17:28 +0530

    Commit Mobile App Service Created

---
+++ Core/Entities/IPIRReportingInformation.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class IPIRReportingInformation:BaseEntity
+    {
+        [Key]
+        public long ReportinginformationID { get; set; }
+        public long IpirAppID { get; set; }
+        public  string? IssueDescription { get; set; }
+        public bool ReportBy {  get; set; }
+        public  long? IssueRelatedTo { get; set; }
+        [NotMapped]
+        public IEnumerable<long?> AssignToIds { get; set; } = new List<long?>();
+        [NotMapped]
+        public string? Message { get; set; }
+    }
+}

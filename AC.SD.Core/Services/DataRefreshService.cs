e58b5320dcf505d7e22e6584cf1d4cca1d9718ac
Vijay <Vijay@LAPTOP-HDA4JTJH>
2024-09-05 15:31:27 +0530

    Commit

---
+++ AC.SD.Core/Services/DataRefreshService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class DataRefreshService
+{
+    public event Func<Task> OnRefreshRequested;
+
+    public async Task RequestRefreshAsync()
+    {
+        if (OnRefreshRequested != null)
+        {
+            await OnRefreshRequested.Invoke();
+        }
+    }
+}

86d78a59c673006d5cf8f77cd82ee537870f58a7
Vijay <Vijay@LAPTOP-HDA4JTJH>
2024-09-11 07:38:38 +0530

    commit

--- AC.SD.Core/Services/ResourceCollection.cs
+++ AC.SD.Core/Services/ResourceCollection.cs
@@ -13,9 +13,16 @@ namespace AC.SD.Core.Services
         {
             return new List<Resource>() {
                 new Resource() { Id=0 , Name="Email", GroupId=100, BackgroundCss="dxbl-blue-color", TextCss="text-white" },
-                new Resource() { Id=1 , Name="Todo", GroupId=101, BackgroundCss="dxbl-pink-color", TextCss="text-white" },
-                new Resource() { Id=2 , Name="Appointments", GroupId=100, BackgroundCss="dxbl-purple-color", TextCss="text-white" }               
+                new Resource() { Id=1 , Name="Todo", GroupId=100, BackgroundCss="dxbl-pink-color", TextCss="text-white" },
+                new Resource() { Id=2 , Name="Appointments", GroupId=102, BackgroundCss="dxbl-purple-color", TextCss="text-white" }               
             };
-        }       
+        }
+        public static List<Resource> GetResourceGroups()
+        {
+            return new List<Resource>() {
+                new Resource() { Id=100, Name="Scheduler Filter", IsGroup=true }
+                //new Resource() { Id=101, Name="Tags", IsGroup=true }
+            };
+        }
     }
 }

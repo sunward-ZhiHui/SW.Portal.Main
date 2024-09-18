8110ae12eacb40334842163d9113ed8ba9c1f767
sivaranjani <sivaranjaniditto@gmail.com>
2024-09-18 17:18:28 +0530

    Commit QCApp Service Created

---
+++ Application/Queries/CreateQCTimesheetQuery.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries
+{
+    public class CreateQCTimesheetQuery : TimeSheetForQC, IRequest<long>
+    {
+    }
+    public class UpdateQCTimesheetQuery : TimeSheetForQC, IRequest<long>
+    {
+
+    }
+}
--- SW.Portal.Solutions/Models/LoginModel.cs
+++ SW.Portal.Solutions/Models/LoginModel.cs
@@ -8,7 +8,7 @@
     public class FbOutputCartonsModels
     {
         public long FbOutputCartonID { get; set; }
-       
+
     }
     public class DeviceTkenModel
     {
@@ -19,8 +19,16 @@
     public class NotificationAllModel
     {
         public List<string>? tokens { get; set; } = new List<string>();
-        public string? titles { get; set; } 
+        public string? titles { get; set; }
         public string? message { get; set; }
         public string? housturl { get; set; }
+    }
+    public class UploadModel
+    {
+        public Guid SessionId { get; set; }
+        public long? addedByUserId { get; set; }
+        public IFormFile File { get; set; }
+        public string? NewFilename { get; set; }
+        public IFormCollection Files { get; set; }
     }
 }

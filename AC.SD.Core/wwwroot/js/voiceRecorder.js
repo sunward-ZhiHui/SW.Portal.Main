f4c47992935ac2de7460dc622abda09033525c45
Vijay <Vijay@LAPTOP-HDA4JTJH>
2025-01-11 08:21:24 +0530

    commit

---
+++ AC.SD.Core/wwwroot/js/voiceRecorder.js
@@ -0,0 +1,27 @@
+﻿let mediaRecorder;
+let audioChunks = [];
+
+window.startVoiceRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorder = new MediaRecorder(stream);
+
+    audioChunks = []; // Reset chunks for a new recording
+    mediaRecorder.ondataavailable = event => {
+        audioChunks.push(event.data);
+    };
+
+    mediaRecorder.start();
+};
+
+window.stopVoiceRecording = () => {
+    return new Promise((resolve) => {
+        mediaRecorder.onstop = () => {
+            const blob = new Blob(audioChunks, { type: "audio/wav" });
+            const audioUrl = URL.createObjectURL(blob);
+
+            resolve(audioUrl); // Return the blob URL for playback
+        };
+
+        mediaRecorder.stop();
+    });
+};
